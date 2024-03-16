c5e3d24765ef086890a47fe7f23077c215a3052c
Jim Lemmers <jim.lemmers@gmail.com>
2024-03-16 11:29:12 +0100

    Add dialog to the modal

--- templates/phx_web/components/modal.ex
+++ templates/phx_web/components/modal.ex
@@ -1,12 +1,20 @@
 defmodule <%= @web_namespace %>.Components.Modal do
   use <%= @web_namespace %>.Component
 
+  prop id, :string, required: true
+  prop show, :boolean
   slot default
+
   def render(assigns) do
     ~F"""
-      <div class="modal">
-        <#slot />
-      </div>
+      <dialog open={@show}>
+        <article>
+          <header>
+            <button autofocus phx-click={JS.toggle_attribute({"open", "false"}, to: "##{@id}")}>Close</button>
+          </header>
+          <#slot />
+        </article>
+      </dialog>
     """
   end
 end
