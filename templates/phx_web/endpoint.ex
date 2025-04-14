8c508785b7fe560c4ccfe0715ba89cade09fbe7f
Jim Lemmers <jim.lemmers@gmail.com>
2025-04-14 21:59:08 +0200

    Add compression to Sockets by default

--- templates/phx_web/endpoint.ex
+++ templates/phx_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule <%= @endpoint_module %> do
   ]
 
   socket "/live", Phoenix.LiveView.Socket,
-    websocket: [connect_info: [session: @session_options]],
+    websocket: [connect_info: [session: @session_options], compress: true],
     serializer: [{Punkix.MsgpackSerializer, "2.0.0"}]
 
   # Serve at "/" the static files from "priv/static" directory.
