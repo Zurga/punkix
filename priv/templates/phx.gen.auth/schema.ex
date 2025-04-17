d3a9476327b3e4c3edc016b64c401ccaf45378c0
Jim Lemmers <jim.lemmers@gmail.com>
2025-04-17 22:25:51 +0200

    Add assocs to gen.auth

--- priv/templates/phx.gen.auth/schema.ex
+++ priv/templates/phx.gen.auth/schema.ex
@@ -1,5 +1,6 @@
 defmodule <%= inspect schema.module %> do
-  use <%= Mix.Phoenix.base() %>.Schema
+  use <%= Mix.Phoenix.base() %>.Schema<%= if schema.assocs != [] do %>
+  alias <%= Mix.Phoenix.base() %>.Schemas.{<%= Punkix.Schema.assoc_aliasses(schema) %>}<% end %>
 
 <%= if schema.binary_id do %>  @primary_key {:id, :binary_id, autogenerate: true}
   @foreign_key_type :binary_id<% end %>
@@ -9,6 +10,7 @@ defmodule <%= inspect schema.module %> do
     field :hashed_password, :string, redact: true
     field :confirmed_at, :naive_datetime
 
+    <%= Punkix.Schema.format_assocs(schema) %>
     timestamps(<%= if schema.timestamp_type != :naive_datetime, do: "type: #{inspect schema.timestamp_type}" %>)
   end
 end
