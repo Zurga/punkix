868edf11c7ce01d52481534c792be0e0c753c132
Jim Lemmers <jim.lemmers@gmail.com>
2025-05-21 16:49:11 +0200

    Make migration compatible with nillify_all

--- priv/templates/phx.gen.schema/migration.exs
+++ priv/templates/phx.gen.schema/migration.exs
@@ -5,7 +5,7 @@ defmodule <%= inspect schema.repo %>.Migrations.Create<%= Macro.camelize(schema.
     create table(:<%= schema.table %><%= if schema.binary_id do %>, primary_key: false<% end %><%= if schema.prefix do %>, prefix: :<%= schema.prefix %><% end %>) do
 <%= if schema.binary_id do %>      add :id, :binary_id, primary_key: true
 <% end %><%= for {k, v} <- schema.attrs do %>      add <%= inspect k %>, <%= inspect Mix.Phoenix.Schema.type_for_migration(v) %><%= schema.migration_defaults[k] %>
-<% end %><%= for %{assoc_fun: :belongs_to} = assoc <- schema.assocs do %>      add <%= inspect(assoc.key) %>, references(<%= inspect(assoc.assoc_table) %>, on_delete: :nilify_all<%= if schema.binary_id do %>, type: :binary_id<% end %>)<%= if assoc.required do %>, null: false<% end %>
+<% end %><%= for %{assoc_fun: :belongs_to} = assoc <- schema.assocs do %>      add <%= inspect(assoc.key) %>, references(<%= inspect(assoc.assoc_table) %>, on_delete: :nilify_all<%= if schema.binary_id do %>, type: :binary_id<% end %>)
 <% end %>
       timestamps(<%= if schema.timestamp_type != :naive_datetime, do: "type: #{inspect schema.timestamp_type}" %>)
     end
