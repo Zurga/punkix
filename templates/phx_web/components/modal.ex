952ade7492298babc4005399b5dd17e55660a3a3
Jim Lemmers <jim.lemmers@gmail.com>
2025-06-10 22:00:00 +0200

    Fixup modal component

--- templates/phx_web/components/modal.ex
+++ templates/phx_web/components/modal.ex
@@ -6,18 +6,26 @@ defmodule <%= @web_namespace %>.Components.Modal do
   prop on_cancel, :fun
   slot default
   slot title
+  slot footer
 
   def render(assigns) do
     ~F"""
-      <dialog open={@show}>
+      <dialog {=@id} open={@show}>
         <article>
           <header>
-            <button rel="prev" phx-click={@on_cancel || JS.toggle_attribute({"open", "false"}, to: "##{@id}")}>Close</button>
+            <button type="button" rel="prev" :on-click={@on_cancel || open_or_close(@id)}>Close</button>
             <#slot {@title} />
           </header>
           <#slot />
+        <footer :if={slot_assigned?(@footer)}>
+          <#slot {@footer} />
+        </footer>
         </article>
       </dialog>
     """
+  end
+
+  def open_or_close(dialog_id) do
+    JS.toggle_attribute({"open", "true", "false"}, to: "##{dialog_id}")
   end
 end
