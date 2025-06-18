9a5610baf8b680f4936671efe2e9bd7b33f51735
Jim Lemmers <jim.lemmers@gmail.com>
2025-06-18 15:25:19 +0200

    Add support to preserve details element

--- assets/js/punkix/index.js
+++ assets/js/punkix/index.js
@@ -1,17 +1,5 @@
-import { Encoder, Decoder } from "@msgpack/msgpack";
 import PunkixHooks from "../_hooks";
 
-const encoder = new Encoder({ ignoreUndefined: true });
-const decoder = new Decoder();
-
-const encode =  (payload, callback) => {
-		// This is needed for fileUpload to work
-		if (payload.payload instanceof ArrayBuffer) {
-			payload.payload = new Uint8Array(payload.payload);
-		}
-		callback(encoder.encode(payload));
-	}
-const	decode = (payload, callback) => callback(decoder.decode(payload))
 /**
  * Controls `<details>` state on the client when LiveView updates the DOM.
  *
